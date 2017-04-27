f8c37f8cae7b3234ff6ca33d539410e032474500
cavra <coryavra@gmail.com>
2017-04-27 16:25:33 -0500

    iFrame page tweaks

--- js/iframe.js
+++ js/iframe.js
@@ -1,3 +1,7 @@
 function iFrameResize() { 
 	document.getElementById("contentFrame").style.height = document.getElementById("contentFrame").contentWindow.document.body.scrollHeight + "px"; 
 }
+
+function setSource(url){
+    document.getElementById('contentFrame').src = url;
+}
