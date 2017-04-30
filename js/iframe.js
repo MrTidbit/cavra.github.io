ab6817aa2fd37ab3070e6b40a3a7ddbbb18e323c
cavra <coryavra@gmail.com>
2017-04-30 13:42:01 -0500

    background is transparent

--- js/iframe.js
+++ js/iframe.js
@@ -1,7 +1,8 @@
 function iFrameResize() { 
-	document.getElementById("contentFrame").style.height = document.getElementById("contentFrame").contentWindow.document.body.scrollHeight + "px"; 
+	document.getElementById("contentFrame").style.height = document.getElementById("contentFrame").contentWindow.document.body.scrollHeight + "px";
 }
 
 function setSource(url){
     document.getElementById('contentFrame').src = url;
+    iFrameResize();
 }
