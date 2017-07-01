4927da019a3cecf754439ae4c9cc5a8fc87030f2
cavra <coryavra@gmail.com>
2017-07-01 00:20:03 -0500

    Added Typed.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,25 @@
 var contentContainer = document.getElementById('contentContainer');
 var contentFrame = document.getElementById('contentFrame');
 
-function setSource(url){
-	$(function() {
-		$(document.getElementById('contentContainer')).animate({opacity: 0}, 1000, function() {
-			contentFrame.src = url;
-		});
-	});
 
-	$(function() {
-		$(document.getElementById('contentContainer')).animate({opacity: 1}, 1000);
-	});
-}
+var typed = new Typed('.write', {
+  strings: ["Hello, World."],
+  typeSpeed: 40, // typing speed
+  startDelay: 0,
+  backDelay: 750, // pause before backspacing
+  smartBackspace: true,
+  loop: false, // loop on or off (true or false)
+  loopCount: false, // number of loops, false = infinite
+  callback: function(){ } // call function after typing is done
+});
 
-// Do I really want this?
-function iFrameResize() { 
-	document.getElementById('contentContainer').style.height = document.getElementById('contentFrame').contentWindow.document.body.scrollHeight + "px";
-}
+var typed = new Typed('.write2', {
+  strings: ["My name is Cory Avra"],
+  typeSpeed: 40, // typing speed
+  startDelay: 2000,
+  backDelay: 750, // pause before backspacing
+  smartBackspace: true,
+  loop: false, // loop on or off (true or false)
+  loopCount: false, // number of loops, false = infinite
+  callback: function(){ } // call function after typing is done
+});
