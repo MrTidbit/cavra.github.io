bfd45fb80ead2980b4ce27d05e2986941d930e72
cavra <coryavra@gmail.com>
2017-07-17 22:52:37 -0500

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -16,7 +16,19 @@ var typed = new Typed('.write', {
 var typed = new Typed('.write2', {
   strings: ["My name is Cory Avra"],
   typeSpeed: 40, // typing speed
-  startDelay: 2000,
+  startDelay: 1500,
+  backDelay: 750, // pause before backspacing
+  smartBackspace: true,
+  loop: false, // loop on or off (true or false)
+  loopCount: false, // number of loops, false = infinite
+  callback: function(){ } // call function after typing is done
+});
+
+var typed = new Typed('.write3', {
+  strings: ["This website is crap", "This website is ^1000 under construction"],
+  typeSpeed: 50, // typing speed
+  backSpeed: 200, // typing speed
+  startDelay: 3000,
   backDelay: 750, // pause before backspacing
   smartBackspace: true,
   loop: false, // loop on or off (true or false)
