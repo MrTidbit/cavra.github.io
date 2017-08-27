9af3fffcec6b4c90979ffd8539cc24ca58b12fcc
cavra <coryavra@gmail.com>
2017-08-27 13:37:26 -0500

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,37 +1,12 @@
-var contentContainer = document.getElementById('contentContainer');
-var contentFrame = document.getElementById('contentFrame');
 
-
-var typed = new Typed('.write', {
-  strings: ["Hello, World."],
-  typeSpeed: 40, // typing speed
-  startDelay: 0,
-  backDelay: 750, // pause before backspacing
-  smartBackspace: true,
-  loop: false, // loop on or off (true or false)
-  loopCount: false, // number of loops, false = infinite
-  callback: function(){ } // call function after typing is done
-});
-
-var typed = new Typed('.write2', {
-  strings: ["My name is Cory Avra"],
-  typeSpeed: 40, // typing speed
-  startDelay: 1500,
-  backDelay: 750, // pause before backspacing
-  smartBackspace: true,
-  loop: false, // loop on or off (true or false)
-  loopCount: false, // number of loops, false = infinite
-  callback: function(){ } // call function after typing is done
-});
-
-var typed = new Typed('.write3', {
-  strings: ["This website is crap", "This website is ^1000 under construction"],
-  typeSpeed: 50, // typing speed
-  backSpeed: 200, // typing speed
-  startDelay: 3000,
-  backDelay: 750, // pause before backspacing
-  smartBackspace: true,
-  loop: false, // loop on or off (true or false)
-  loopCount: false, // number of loops, false = infinite
-  callback: function(){ } // call function after typing is done
+var typed = new Typed('.type-welcome', {
+  strings: ["^500 developer", "^500 designer", "^500 creator"],
+  typeSpeed: 30,
+  backSpeed: 100,
+  startDelay: 300,
+  backDelay: 1500,
+  smartBackspace: false,
+  loop: false,
+  loopCount: false,
+  callback: function(){ }
 });
