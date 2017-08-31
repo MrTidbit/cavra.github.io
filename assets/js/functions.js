8f42b955de7e32d542c37f732120e8ed72e84605
cavra <coryavra@gmail.com>
2017-08-31 12:45:52 -0500

    Added sections, filler text

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,8 @@
 
 var typed = new Typed('.type-welcome', {
-  strings: ["^500 developer", "^500 designer", "^500 creator"],
+  strings: ["^500 Web Developer", "^500 Mobile Developer", "Software Engineer"],
   typeSpeed: 30,
-  backSpeed: 100,
+  backSpeed: 80,
   startDelay: 300,
   backDelay: 1500,
   smartBackspace: false,
